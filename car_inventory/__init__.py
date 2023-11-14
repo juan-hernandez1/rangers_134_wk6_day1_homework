7f435f25c978bb83a933c51b4cf077dca78de782
juan-hernandez1 <juan.c.hernandez.312@gmail.com>
2023-11-14 17:13:04 -0600

    second commit

--- car_inventory/__init__.py
+++ car_inventory/__init__.py
@@ -1,8 +1,32 @@
 from flask import Flask 
-from .blueprints.site.routes import site 
+from flask_migrate import Migrate 
+
+
+#internal imports 
+from .blueprints.site.routes import site
+from .blueprints.auth.routes import auth 
+from config import Config 
+from .models import login_manager, db 
 
 #instantiating our Flask app
-app = Flask(__name__) #passing in the __name__ variable which just takes the name of the folder we're in
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+#wrap our app in login_manager so we can use it wherever in our app
+login_manager.init_app(app)
+login_manager.login_view = 'auth.sign_in' 
+login_manager.login_message = "Your Log In Is Required!"
+login_manager.login_message_category = 'warning'
+
+
+
+
+
 app.register_blueprint(site)
+app.register_blueprint(auth)
 
 
+#instantiating our datbase & wrapping our app
+db.init_app(app)
+migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv # allows us to load our environment variables (va
 
 
 # establish our base directory so whenever we use "." to reference any location in our app it knows we are referencing rangers_shop folder
-basedir = os.pathabsparth(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 # need to establish where our environment variables are coming from (this file will be hidden from github)
@@ -27,3 +27,4 @@ class Config():
     FLASK_ENV = os.environ.get('FLASK_ENV')
     FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Literally whatever you want as long as its a string. Cool Beans'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
